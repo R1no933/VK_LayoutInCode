11519c2f4031592ef5ab8798b2a99bf8dd2cf5a4
R1no933 <104028198+R1no933@users.noreply.github.com>
2022-05-29 15:22:15 +0300

    Initial Commit

---
+++ VK_LayoutInCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VK_LayoutInCode
+//
+//  Created by Dmitriy Baskakov on 29.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
